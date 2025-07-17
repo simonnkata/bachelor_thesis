a109dd318651327491089baa3b00ad63431665bf
Simon R. Nkata <simon.ikpabi.nkata@ims.fraunhofer.de>
2025-07-17 16:24:03 +0200

    Changed filter function, added new filter options

--- filter.py
+++ filter.py
@@ -1,12 +1,34 @@
-from scipy.signal import cheby2, filtfilt
+from scipy.signal import cheby2, butter, cheby1, ellip, bessel, firwin, filtfilt
 import pandas as pd
-
+import numpy as np
 
 def cheby2_filter(fs, cutoff, order, rs, df):
-    normalised_cutoff = cutoff / (0.5 * fs)
-    b, a = cheby2(N=order, rs=rs, Wn=normalised_cutoff, btype='low', analog=False)
-    filtered_df = pd.DataFrame()
-    filtered_df['R'] = filtfilt(b, a, df['R'])
-    filtered_df['G'] = filtfilt(b, a, df['G'])
-    filtered_df['B'] = filtfilt(b, a, df['B'])
-    return filtered_df
+    norm = cutoff / (0.5 * fs)
+    b, a = cheby2(order, rs, norm, btype='low')
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
+
+def butter_filter(fs, cutoff, order, rs, df):
+    norm = cutoff / (0.5 * fs)
+    b, a = butter(order, norm, btype='low')
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
+
+def cheby1_filter(fs, cutoff, order, rs, df):
+    norm = cutoff / (0.5 * fs)
+    b, a = cheby1(order, 0.5, norm, btype='low')  # using rs as rp
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
+
+def ellip_filter(fs, cutoff, order, rs, df):
+    norm = cutoff / (0.5 * fs)
+    b, a = ellip(order, 0.5, rs, norm, btype='low')  # using rs for both rp and rs
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
+
+def bessel_filter(fs, cutoff, order, rs, df):
+    norm = cutoff / (0.5 * fs)
+    b, a = bessel(order, norm, btype='low', norm='phase')
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
+
+def fir_filter(fs, cutoff, order, rs, df):
+    norm = cutoff / (0.5 * fs)
+    b = firwin(numtaps=order + 1, cutoff=norm)
+    a = 1
+    return pd.DataFrame({c: filtfilt(b, a, df[c]) for c in ['R', 'G', 'B']})
--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from filter import cheby2_filter
+from filter import cheby2_filter, bessel_filter, butter_filter, cheby1_filter, ellip_filter, fir_filter
+import statistics
+import copy
 
 mapping = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g'}
 
@@ -51,12 +53,35 @@ def plot_all(folder, _data):
             plot_and_save(recording, str(subject_number + 1), mapping[index], directory)
 
 
+def snr(original, filtered):
+    noise = original - filtered
+    signal_power = np.mean(original ** 2)
+    noise_power = np.mean(noise ** 2)
+    _snr = 10 * np.log10(signal_power / noise_power)
+    return _snr
+
+
+def total_snr(original, filtered):
+    snr_values = []
+    for subject_o, subject_f in zip(original, filtered):
+        for entry_o, entry_f in zip(subject_o, subject_f):
+            snr_red = snr(entry_o['R'], entry_f['R'])
+            snr_blue = snr(entry_o['B'], entry_f['B'])
+            snr_green = snr(entry_o['G'], entry_f['G'])
+            snr_values.extend([snr_red, snr_blue, snr_green])
+    print(f"Mean: {np.mean(snr_values)}, Standard Deviation: {statistics.stdev(snr_values)}")
+    return [np.mean(snr_values), statistics.stdev(snr_values)]
+
+
 def filter_data(_data):
     for subject in _data:
         for i in range(len(subject)):
-            subject[i] = cheby2_filter(30, 3, 4, 20, subject[i])
+            subject[i] = cheby2_filter(30, 3, 4, 40, subject[i])
+    return _data
 
 
 data = load_data()
-#filter_data(data)
-plot_all('figures_v2', data)
+data_copy = copy.deepcopy(data)
+filtered_data = filter_data(data_copy)
+total_snr(data, filtered_data)
+# plot_all('figures_v2', data)
