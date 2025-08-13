abb350d736e76fab9c9084ee20f702ef2eaa19bd
Simon R. Nkata <simon.ikpabi.nkata@ims.fraunhofer.de>
2025-08-13 15:06:04 +0200

    Wrote functions to split recording into cycles, find fudicial points. Also extract first metrics.

--- features.py
+++ features.py
@@ -9,11 +9,14 @@ import pandas as pd
 import numpy as np
 from scipy.signal import find_peaks
 from validation import heart_rate
-Cfrom preprocessing import load_pos_filter_plot, split_data
+from preprocessing import load_pos_filter_plot, split_data
 import time
+import matplotlib.pyplot as plt
+
 # Fudicial points: Onset, Systolic Peak, Max Slope, Dicrotic Notch, Diastolic Peak
 fs = 30
 classes = {0: 'baseline', 1: 'full', 2: 'moderate', 3: 'light'}
+
 
 def find_pulse_rate_variability(entry):
     recording = entry['signal']
@@ -39,6 +42,82 @@ def find_rise_time(entry):
 def find_decay_time(entry):
     return 0
 
+
+def split_cycles(recording):
+    expected_distance = int(fs * 60 / heart_rate(recording, fs))
+    peaks, _ = find_peaks(recording, distance=expected_distance)
+    inverted = -recording
+    min_indices, _ = find_peaks(inverted, distance=expected_distance)
+
+    # Each min_index is an onset
+    onsets = min_indices
+    '''
+    plt.plot(recording)
+    plt.plot(peaks, recording[peaks], 'rx')
+    plt.plot(onsets, recording[onsets], 'go')
+    plt.title("rPPG Signal with Detected Cycles")
+    plt.show()
+    '''
+    result = []
+    for i in range(1, len(onsets)):
+        cycle = recording[onsets[i - 1]:onsets[i]]
+        result.append(cycle)
+    return result
+
+
+def find_fiducial_points(cycle):
+    systolic_peak = np.argmax(cycle)
+    dicrotic_notch = None
+    dicrotic_peak = None
+    for i in range(systolic_peak + 1, len(cycle)):
+        if cycle[i] > cycle[i - 1]:
+            dicrotic_notch = i
+            break
+    if dicrotic_notch:
+        for i in range(dicrotic_notch + 1, len(cycle)):
+            if cycle[i] < cycle[i - 1]:
+                dicrotic_peak = i
+                break
+    return [systolic_peak, dicrotic_notch, dicrotic_peak]
+
+
+def fiducial_point_features(recording):
+    cycles = split_cycles(recording)
+    cycle_features = []
+    for cycle in cycles:
+        cycle_feature = {}
+        on = cycle[0]
+        sp_i, dn_i, dp_i = find_fiducial_points(cycle)
+        sp, dn, dp = cycle[sp_i], cycle[dn_i], cycle[dp_i]
+        pulse_interval = len(cycle) / fs
+        cycle_feature['pulse_interval'] = pulse_interval
+        if sp:
+            # systolic with
+            value_at_50 = on + 0.5 * (sp - on)
+            segment = cycle[:sp_i+1]
+            index_at_50 = min(range(len(segment)), key=lambda i: abs(segment[i] - value_at_50))
+            systolic_width = (sp_i-index_at_50)/fs
+
+            # systolic_peak_time
+            systolic_peak_time = sp_i/fs
+
+            cycle_feature['systolic_width'] = systolic_width
+            cycle_feature['systolic_peak_time'] = systolic_peak_time
+        if dn:
+            diastolic_width = 0
+            systolic_time = 0
+            diastolic_time = 0
+            cycle_feature['diastolic_width'] = diastolic_width
+            cycle_feature['systolic_time'] = systolic_time
+            cycle_feature['diastolic_time'] = diastolic_time
+        if dp:
+            time_delay = 0
+            diastolic_peak_time = 0
+            cycle_feature['time_delay'] = time_delay
+            cycle_feature['diastolic_peak_time'] = diastolic_peak_time
+    # Do analysis with cycle_features
+
+
 def extract():
     data = load_pos_filter_plot()
     df = split_data(data)
@@ -60,6 +139,8 @@ def extract():
     })
     print(f'We are working with {len(features_df)} rows')
     return features_df
+
+
 start = time.time()
 extract()
 end = time.time()
